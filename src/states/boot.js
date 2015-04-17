6400bbf7e0940729da435859e78e83232ea934da
Jacek Miecznikowski <jacekmiecznikowski@outlook.com>
2015-04-17 11:22:49 +0200

    update of some files + boot.js structure

--- src/states/boot.js
+++ src/states/boot.js
@@ -1,1 +1,19 @@
+var Game = {};
 
+Game.Boot = function (game) {
+
+};
+
+Game.Boot.prototype = {
+    init: function () {
+
+    },
+    preload: function() {
+
+    },
+    create: function() {
+
+    }
+
+
+};
