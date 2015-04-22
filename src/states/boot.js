6de48e0b4fc5ba12336135e419d46481cb45d695
Jacek Miecznikowski <jacekmiecznikowski@outlook.com>
2015-04-22 15:21:01 +0200

    simple loading scene

--- src/states/boot.js
+++ src/states/boot.js
@@ -6,9 +6,10 @@ Game.Boot = function (game) {
 
 Game.Boot.prototype = {
     init: function () {
-
+        this.game.stage.backgroundColor = '#e2e2e2';
     },
     preload: function() {
+        this.load.image('progressBar', 'assets/gfx/progress_bar.png');
     },
     create: function() {
         this.physics.startSystem( Phaser.Physics.ARCADE );
